690bf63843d2559d872bb1b3abd37c886fc568be
thomas hindermann <thomas.hindermann@m-f.ch>
2025-06-23 13:49:53 +0200

    Using in Client

---
+++ Grpc.Client/Program.cs
@@ -0,0 +1,10 @@
+﻿using Grpc.Net.Client;
+using Grpc.Shared;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7016");
+
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "YourName" });
+
+Console.WriteLine(reply.Message);
